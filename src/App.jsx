9b5663f773b111af9a8f3d98bdad36d25e3921b4
RGuio007 <raymezza@gmail.com>
2024-02-02 14:25:59 -0500

    simple react clock

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Clock from "./Clock.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <Clock />
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+class Reloj extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.updatehour(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  updatehour() {
+    this.setState({
+      time: new Date().toLocaleTimeString(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.time}</h1>
+      </div>
+    );
+  }
+}
+
+export default Reloj;
